77ad82e99c9558382555a9c341b8e19e0a2ec20f
hyeonwook <lastindus@gmail.com>
2020-11-24 21:05:46 +0900

    Initial commit

---
+++ src/main/java/com/example/FLC/FinanceLikeCoffee/FinanceLikeCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.FLC.FinanceLikeCoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceLikeCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceLikeCoffeeApplication.class, args);
+	}
+
+}
