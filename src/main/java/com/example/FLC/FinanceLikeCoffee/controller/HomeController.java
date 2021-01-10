02f7e36f2d32b7eb802feeabd70e1b40c7e838f0
hyeonwook <lastindus@gmail.com>
2021-01-10 15:55:32 +0900

    Home page FAQ add

--- src/main/java/com/example/FLC/FinanceLikeCoffee/controller/HomeController.java
+++ src/main/java/com/example/FLC/FinanceLikeCoffee/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.FLC.FinanceLikeCoffee.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,18 +9,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
+
     @GetMapping("/")
     public String home(Model model) {
+        logger.info("home page assessed.......");
         return "home";
     }
 
     @GetMapping("/about")
     public String about(Model model) {
+        logger.info("home page assessed.......");
         return "about";
     }
 
     @GetMapping("/FAQ")
     public String FAQ(Model model) {
+        logger.info("home page assessed.......");
         return "FAQ";
     }
 }
